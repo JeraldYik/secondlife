768934979ba742169451dca1c7f0f4a864f4bd62
Jerald Yik <jerald@Jeralds-MacBook-Pro.local>
2018-11-01 02:53:53 +0800

    compiled everything tgt 01/11/18

---
+++ SecondLife (Recyclables)/app/src/main/java/markwei/secondlifefinal/CorrugatedCardboard.java
@@ -0,0 +1,9 @@
+package markwei.secondlifefinal;
+
+public abstract class CorrugatedCardboard implements Recyclables{
+
+    public abstract double weight();
+    public abstract int quantity();
+    public abstract String recyclablerequirements();
+
+}
