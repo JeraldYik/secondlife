12b48542486eadfdee2321a0e0b8c32015580584
omofish <jason_lzx@hotmail.com>
2018-11-10 03:32:41 +0800

    modified recyclable interface

--- SecondLife (Maps - Main)/app/src/main/java/com/example/xqlim/secondlife/HistoryFolder/HistoryFragment.java
+++ SecondLife (Maps - Main)/app/src/main/java/com/example/xqlim/secondlife/HistoryFolder/HistoryFragment.java
@@ -73,6 +73,8 @@ public class HistoryFragment extends Fragment {
         recycledItems.add(new MetalTin(5, "tins"));
         recycledItems.add(new SmallElectricalAppliance(4.6, "kg"));
         recycledItems.add(new AluminiumDrinkCan(12, "cans"));
+        recycledItems.add(new MetalTin(2, "tins"));
+        recycledItems.add(new SmallElectricalAppliance(3.1, "kg"));
 
     }
 
