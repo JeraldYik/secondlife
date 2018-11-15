92cd4f8d7492dc5dc7827acd9ea63aee63ef978a
Lim Xiao Qi <xqlim1996@gmail.com>
2018-11-15 11:43:22 +0800

    fiex

--- SecondLife (Maps - Main)/app/src/main/java/com/example/xqlim/secondlife/HistoryFolder/HistoryManager.java
+++ SecondLife (Maps - Main)/app/src/main/java/com/example/xqlim/secondlife/HistoryFolder/HistoryManager.java
@@ -11,6 +11,7 @@ import com.example.xqlim.secondlife.RecyclablesFolder.Recyclable;
 import com.example.xqlim.secondlife.RecyclablesFolder.SmallElectricalAppliance;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class HistoryManager {
 
@@ -24,6 +25,7 @@ public class HistoryManager {
 
     private HistoryManager(){
         this.recycledItems = new ArrayList<>();
+        initializeData();
     }
 
     public static HistoryManager getInstance(){
@@ -42,15 +44,61 @@ public class HistoryManager {
     }
 
     public void addHist(Recyclable recyclable) {
-        for (Recyclable r : recycledItems) {
-            if (r.getName() == recyclable.getName()) {
-                double qty = r.getQuantity() + recyclable.getQuantity();
-                r.setQuantity(qty);
+        Boolean match = false;
+        int index = 999;
+        for (int i = 0; i<recycledItems.size(); i++){
+            Log.d(TAG, "Name: " + recycledItems.get(i).getName());
+
+
+            if (recycledItems.get(i).getName() == recyclable.getName()){
+                index = i;
+                match = true;
+                Log.d(TAG, "match! name is + " + recyclable.getName());
                 break;
             }
-            else
-                recycledItems.add(r);
         }
+
+
+        if (match) {
+            Log.d(TAG, "match, replacing qty");
+            Recyclable recyclable_exist = recycledItems.get(index);
+            recyclable_exist.setQuantity(recyclable_exist.getQuantity() + recyclable.getQuantity());
+        }
+        else {
+            Log.d(TAG, recyclable.getName() + "no match, adding new");
+            index = recycledItems.size();
+            recycledItems.add(index, recyclable);
+        }
+
+
+
+
+//        Iterator<Recyclable> iter = recycledItems.iterator();
+//
+//        while (iter.hasNext()) {
+//            Recyclable re = iter.next();
+//            String name = re.getName();
+//
+//            if (name == recyclable.getName()){
+//                re.setQuantity(re.getQuantity()+recyclable.getQuantity());
+//            }
+//            else{
+//
+//            }
+//            recycledItems.add(re);
+//        }
+
+//
+//
+//        for (Recyclable r : recycledItems) {
+//            if (r.getName() == recyclable.getName()) {
+//                double qty = r.getQuantity() + recyclable.getQuantity();
+//                r.setQuantity(qty);
+//                break;
+//            }
+//            else
+//                recycledItems.add(r);
+//        }
     }
 
     public ArrayList<Recyclable> getRecycledItems() {
